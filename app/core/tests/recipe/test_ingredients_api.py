8cc8c350540d879bc2b1b7fab2f162519816c9f9
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2021-05-15 19:40:29 +0600

    Add filter in tags & Ingredients & test

--- app/core/tests/recipe/test_ingredients_api.py
+++ app/core/tests/recipe/test_ingredients_api.py
@@ -6,7 +6,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from recipe.models import Ingredient
+from recipe.models import Ingredient,Recipe
 
 from recipe.serializers import IngredientSerializer
 
@@ -85,4 +85,25 @@ class PrivateIngredientsApiTests(TestCase):
         payload = {'name' : ''}
         res = self.client.post(INGREDIENTS_URL,payload)
         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+
+    
+    def test_retrieve_ingredients_assigned_to_recipe(self):
+        """Test filtering ingredients by those assigned to recipes"""
+        ingredient1 = Ingredient.objects.create(user = self.user , name = "Breakfast")
+        ingredient2 = Ingredient.objects.create(user = self.user , name = "Lunch")
+        recipe = Recipe.objects.create(
+            title = 'Cordiander eggs on toast',
+            time_minutes = 10,
+            price = 5.00,
+            user = self.user
+        )
+
+        recipe.ingredients.add(ingredient1)
+        res = self.client.get(INGREDIENTS_URL,{'assigned_only' : 1})
+
+        serializer1 = IngredientSerializer(ingredient1)
+        serializer2 = IngredientSerializer(ingredient2)
+
+        self.assertIn(serializer1.data , res.data)
+        self.assertNotIn(serializer2.data , res.data)
 
